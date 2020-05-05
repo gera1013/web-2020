c33bc9f593a7bfd1f99dc38a79c5a319a0b66463
gera1013 <gera1013@users.noreply.github.com>
2020-05-05 16:57:22 -0600

    Owner viewset

--- ejemplos_django/example/owners/views.py
+++ ejemplos_django/example/owners/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from permissions.services import APIPermissionClassFactory
+from owners.models import Owner
+from owners.serializers import OwnerSerializer
 
-# Create your views here.
+class OwnerViewSet(viewsets.ModelViewSet):
+    queryset = Owner.objects.all()
+    serializer_class = OwnerSerializer
+    permission_classes = (
+        APIPermissionClassFactory(
+            name = 'OwnerPermission',
+            permission_configuration = {
+                'base': {
+                    'create': True,
+                    'list': True,
+                },
+                'instance': {
+                    'retrieve': True,
+                    'destroy': True,
+                    'update': True,
+                    'partial_update': False,
+                }
+            }
+        ),
+    )
